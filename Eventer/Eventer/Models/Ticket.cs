485a21bd91b92f703a424aecb6a883e2119aae7f
Flaysen <you@example.com>
2019-11-13 15:42:32 +0100

    location, ticket class

--- Eventer/Eventer/Models/Ticket.cs
+++ Eventer/Eventer/Models/Ticket.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,30 @@ namespace Eventer.Models
 {
     public class Ticket
     {
+
+        public enum TicketType
+        {
+            normal,
+            premium
+        }
+
+
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public float Pirce { get; set; }
+
+        [Required]
+        public string SerialNumber { get; set; }
+
+        public TicketType Type { get; set; }
+
+        [Required]
+        public int EventId { get; set; }
+
+        [ForeignKey("EventId")]
+        public Event Event { get; set; }
     }
 }
